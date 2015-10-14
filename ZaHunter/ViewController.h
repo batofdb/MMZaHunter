6b419ae7a096ebf32a627730e366241f1fa358b1
Francis Bato <batofdb@gmail.com>
2015-10-14 13:37:09 -0700

    Initial Commit

---
+++ ZaHunter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZaHunter
+//
+//  Created by Francis Bato on 10/14/15.
+//  Copyright © 2015 Francis Bato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZaHunter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZaHunter
+//
+//  Created by Francis Bato on 10/14/15.
+//  Copyright © 2015 Francis Bato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
